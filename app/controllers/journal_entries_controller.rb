b3b0d8a6675193b50c757de650b669d10a21f85a
Andrew Rot <arotc7@gmail.com>
2017-03-14 11:28:10 -0400

    Add journal entry new and create actions

---
+++ app/controllers/journal_entries_controller.rb
@@ -0,0 +1,38 @@
+class JournalEntriesController < ApplicationController
+  before_action :find_journal_entry, only: [:edit, :show, :update]
+
+  def new
+    @entry = JournalEntry.new
+  end
+
+  def create
+    @entry = JournalEntry.new(journal_params)
+    @entry.author = current_user
+
+    if @entry.save
+      flash[:success] = "Journal entry successfully saved"
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def find_journal_entry
+    @entry = current_user.journal_entries.find(params[:id])
+  end
+
+  def journal_params
+    params.require(:journal_entry).permit(:date_for, :content)
+  end
+end
