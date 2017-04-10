fca8ef3ccc9d5d45ed93124bfbdda9c70620162a
Melissa Bazany <melissa.bazany@hope.edu>
2017-04-10 11:42:54 -0400

    Changed title on Resource page and fixed log in function on test

--- spec/features/resources_spec.rb
+++ spec/features/resources_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 describe "resources" do
   it "allows user to see resource panel" do
     user = create(:user)
-    log_in user
+    login_as user
     visit root_path
 
     expect(page).to have_content("Resources")
     click_link "Resources"
 
-    expect(page).to have_content("Resources Checked Out")
+    expect(page).to have_css("h1", text: "Resources")
     expect(current_path).to eq(resources_path)
   end
 end
